dcd5003fdc0f97697d9512185163b792e404d8a7
Duncan Haran <duncan.haran@gmail.com>
2019-08-27 19:11:43 +0000

    user failed login error message, and add-to-cart spec

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,15 +2,16 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-  if user = User.authenticate_with_credentials(params[:email], params[:password])
-    session[:user_id] = user.id
-    redirect_to '/'
-  else
-    redirect_to '/login'
-  end
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      @error = "Your password was incorrect, please try again."
+      render 'sessions/new.html.erb'
+    end
   end
 
-  def destroy
+   def destroy
     session[:user_id] = nil
     redirect_to '/login'
   end
---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+scenario "Can add a product to their cart from root" do
+
+  visit root_path
+
+  click_on('Add', match: :first)
+
+  save_screenshot
+
+  expect(page).to have_content 'My Cart (1)'
+end
+
+end
